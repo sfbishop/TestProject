025f69630307405f3b7cf8c947b8422eca171021
stevenb <stevenfbishop@gmail.com>
2011-11-15 16:25:36 -0400

    Test Commit

---
+++ TestProject/src/com/sb/Something.java
@@ -0,0 +1,16 @@
+package com.sb;
+
+public class Something {
+	int iValue = 0;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	
+	}
+	
+	public int doSomething(){
+		return iValue  = iValue +5;
+	}
+
+}
